d76da80a473244bc855e492d61c97738fa526bb8
Yessine-iut <yessinebenelbey0305@gmail.com>
2022-04-03 16:28:48 +0200

    t

--- node_lucioles_v4.js
+++ node_lucioles_v4.js
@@ -183,7 +183,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-
+console.log("test"+app);
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, '/')));
